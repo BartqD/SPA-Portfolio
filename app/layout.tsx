739d52f4c045e662861e0eb7fcc7db2c49200743
Bartosz <bdrozdek@gmail.com>
2024-03-26 12:56:17 +0100

    feat: add social-bar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { Outfit } from 'next/font/google'
+import { Space_Grotesk } from 'next/font/google'
 
 export const metadata: Metadata = {
 	title: '',
 	description: '',
 }
 
-const outfit = Outfit({ weight: ['400', '700'], subsets: ['latin'] })
+const spaceGrotesk = Space_Grotesk({ weight: ['400','600', '700'], subsets: ['latin'] })
 
 export default function RootLayout({
 	children,
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body className={outfit.className}>{children}</body>
+			<body className={spaceGrotesk.className}>{children}</body>
 		</html>
 	)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,24 @@
 import { Fragment } from 'react'
 import classes from './page.module.scss'
+import SocialBar from '@/components/header/social-bar'
 
 export default function Home() {
 	return (
 		<Fragment>
+			<header className={`${classes.header} ${classes.wrapper} `}>
+				<SocialBar></SocialBar>
+
+			</header>
 			<main>
+				<section></section>
 			</main>
 			<footer>
 				<div className={classes.attribution}>
-					Challenge by <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>Frontend Mentor</a>. Coded by <a href='https://github.com/BartqD'>Bartosz</a>.
+					Challenge by{' '}
+					<a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+						Frontend Mentor
+					</a>
+					. Coded by <a href='https://github.com/BartqD'>Bartosz</a>.
 				</div>
 			</footer>
 		</Fragment>
