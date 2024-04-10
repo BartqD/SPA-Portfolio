485bab032dea0b076e291bee6c11af1c5134e57a
Bartosz <bdrozdek@gmail.com>
2024-04-10 12:36:00 +0200

    feat: Add logic for sending messages to the database.

--- components/sections/contact/contact-form.tsx
+++ components/sections/contact/contact-form.tsx
@@ -1,9 +1,57 @@
 import classes from './contact-form.module.scss'
+import { useState } from 'react'
 import { IoAlertCircleOutline } from 'react-icons/io5'
 
+interface Contact {
+	email: string
+	name: string
+	message: string
+}
+
+async function sendContactData(contact: Contact) {
+	const response = await fetch('/api/contact', {
+		method: 'POST',
+		body: JSON.stringify(contact),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+
+	const data = await response.json()
+
+	if (!response.ok) {
+		throw new Error(data.message || 'Something went wrong')
+	}
+}
+
 const ContactForm: React.FC = () => {
+	const [enteredEmail, setEnteredEmail] = useState<string>('')
+	const [enteredName, setEnteredName] = useState<string>('')
+	const [enteredMessage, setEnteredMessage] = useState<string>('')
+	const [errorMessage, setErrorMessage] = useState<string>('')
+
+	async function sendMessageHandler(event: React.FormEvent) {
+		event.preventDefault()
+
+		const contact: Contact = {
+			email: enteredEmail,
+			name: enteredName,
+			message: enteredMessage,
+		}
+
+		try {
+			await sendContactData(contact)
+			setEnteredEmail('')
+			setEnteredName('')
+			setEnteredMessage('')
+		} catch (error) {
+			setErrorMessage((error as Error).message || 'Something went wrong!')
+			return
+		}
+	}
+
 	return (
-		<form className={classes['contact-form']}>
+		<form onSubmit={sendMessageHandler} className={classes['contact-form']}>
 			<div className={classes['contact-form__group']}>
 				<label htmlFor='name' className={classes['contact-form__label']}></label>
 				<div className={classes['contact-form__group-alert']}>
@@ -14,10 +62,13 @@ const ContactForm: React.FC = () => {
 						placeholder='name'
 						aria-label='name'
 						className={classes['contact-form__input']}
+						required
+						value={enteredName}
+						onChange={e => setEnteredName(e.target.value)}
 					/>
 					{/* <IoAlertCircleOutline className={classes['contact-form__alert']}  /> */}
 				</div>
-					{/* <p className={classes['contact-form__error-message']}>Sorry, invalid format here</p>  */}
+				{/* <p className={classes['contact-form__error-message']}>Sorry, invalid format here</p>  */}
 			</div>
 			<div className={classes['contact-form__group']}>
 				<label htmlFor='email' className={classes['contact-form__label']}></label>
@@ -29,6 +80,9 @@ const ContactForm: React.FC = () => {
 						placeholder='email'
 						aria-label='email'
 						className={classes['contact-form__input']}
+						required
+						value={enteredEmail}
+						onChange={e => setEnteredEmail(e.target.value)}
 					/>
 					{/* <IoAlertCircleOutline className={classes['contact-form__alert']}  /> */}
 				</div>
@@ -44,10 +98,13 @@ const ContactForm: React.FC = () => {
 						rows={6}
 						aria-label='Message'
 						className={`${classes['contact-form__textarea']} ${classes['contact-form__input']} `}
+						value={enteredMessage}
+						onChange={e => setEnteredMessage(e.target.value)}
+						required
 					/>
 					{/* <IoAlertCircleOutline className={classes['contact-form__alert']}  /> */}
 				</div>
-					{/* <p className={classes['contact-form__error-message']}>Sorry, invalid format here</p>  */}
+				{/* <p className={classes['contact-form__error-message']}>Sorry, invalid format here</p>  */}
 			</div>
 			<button type='submit' className={classes['contact-form__button']}>
 				send message
---
+++ lib/db-util.ts
@@ -0,0 +1,19 @@
+import { MongoClient } from 'mongodb'
+
+export async function connectDatabase() {
+	if (!process.env.mongodb_username || !process.env.mongodb_password) {
+		throw new Error('MongoDB username or password is not defined in environment variables.')
+	}
+
+	const username = encodeURIComponent(process.env.mongodb_username)
+	const password = encodeURIComponent(process.env.mongodb_password)
+	const uri = `mongodb+srv://${username}:${password}@cluster0.lzl7hjs.mongodb.net/?retryWrites=true&w=majority`
+
+	const client = await MongoClient.connect(uri)
+	return client
+}
+
+export async function insertContact(client: MongoClient, document: any) {
+	const db = client.db('SpaPortfolio')
+	await db.collection('messages').insertOne(document)
+}
