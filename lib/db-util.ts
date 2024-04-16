ad98e2b61818daf6396630c18ee1d99e5a0c2bc1
Bartosz <bdrozdek@gmail.com>
2024-04-16 12:25:45 +0200

    refactor: database connection

--- lib/db-util.ts
+++ lib/db-util.ts
@@ -1,12 +1,16 @@
 import { MongoClient } from 'mongodb'
 
 export async function connectDatabase() {
-	if (!process.env.mongodb_username || !process.env.mongodb_password) {
-		throw new Error('MongoDB username or password is not defined in environment variables.')
-	}
+	// if (!process.env.mongodb_username || !process.env.mongodb_password) {
+	// 	throw new Error('MongoDB username or password is not defined in environment variables.')
+	// }
 
-	const username = encodeURIComponent(process.env.mongodb_username)
-	const password = encodeURIComponent(process.env.mongodb_password)
+	const username = encodeURIComponent(process.env.mongodb_username!)
+	const password = encodeURIComponent(process.env.mongodb_password!)
+
+	console.log(process.env.mongodb_username)
+	console.log(process.env.mongodb_password)
+	
 	const uri = `mongodb+srv://${username}:${password}@cluster0.lzl7hjs.mongodb.net/?retryWrites=true&w=majority`
 
 	const client = await MongoClient.connect(uri)
