e609f7bb8e15d5f269a5010a509d84654133d3bc
Bartosz <bdrozdek@gmail.com>
2024-04-18 11:14:23 +0200

    refactor: change next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	env: {
-		mongodb_username: process.env.mongodb_username,
-		mongodb_password: process.env.mongodb_password,
-	},
 }
 
 export default nextConfig
